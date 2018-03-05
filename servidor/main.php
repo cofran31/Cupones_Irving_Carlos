30a8c226158baf9ccba6d37703209c29518457e8
Carlos Ortube <cool3000bo@gmail.com>
2018-03-05 19:08:29 -0400

    se modifica el archivo main para pruebas de los patrones

--- servidor/main.php
+++ servidor/main.php
@@ -1,8 +1,16 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+include 'Command/TiendaCommandee.php';
+
+$tienda = new TiendaCommandee();
+/*
+  //[objeto tienda], [tabla bd], [nombre atributos] , [valores de atributos], [orderby]
+  $commandAllProduct = new BuscarTiendaCommand($tienda,'usuario',null,null,null);
+  $commandAllProduct->execute();
+  print_r($tienda->getResponse());
  */
 
+//[objeto tienda], [tabla bd], [nombre atributos] , [valores de atributos], [orderby]
+$starsOff = new BuscarTiendaIdCommand($tienda, 'usuario', array('id'), array(1), null);
+$starsOff->execute();
+print_r($tienda->getResponse());
